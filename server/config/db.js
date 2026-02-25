dde7734d55f28a0ed32dbd3801c39a0585ec8158
SAFI19122002 <safibushy@gmail.com>
2026-02-26 02:58:46 +0530

    Fix db

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("Mongo connected to:", mongoose.connection.host);
    console.log("MongoDB Connected to:", mongoose.connection.name);
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
