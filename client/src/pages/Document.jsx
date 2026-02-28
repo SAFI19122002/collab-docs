4f74726f4bfe36f3e789281c84196bda1f242883
SAFI19122002 <safibushy@gmail.com>
2026-03-01 02:25:38 +0530

    fix(ui): import quill-cursors styles to fix alignment and style the cursor flags to look like opaque pill avatars

--- client/src/pages/Document.jsx
+++ client/src/pages/Document.jsx
@@ -4,6 +4,7 @@ import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { io } from "socket.io-client";
 import QuillCursors from "quill-cursors";
+import "quill-cursors/dist/quill-cursors.css";
 
 import API from "../api/axios";
 import TopBar from "../components/TopBar";
